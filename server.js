95fdc348dd8a92c19b1ff71261ca4e59f5690a3f
maxmalato <max.malato@hotmail.com>
2024-10-06 12:09:11 -0300

    Instalação do CORS

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ import express from "express"
 import publicRoutes from "./routes/public.js"
 import privateRoutes from "./routes/private.js"
 import auth from "./middlewares/auth.js"
+import cors from "cors"
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 // CONF. PORTA
 app.listen(3000, () => console.log("Servidor ligado na porta 3000! 🚀"))
