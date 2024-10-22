6575be62251fdb3b2a9d380a81160ff4915521cf
maxmalato-dell-3567 <max.malato@hotmail.com>
2024-10-22 13:44:58 -0300

    Incluindo o CORS

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import cors from "cors"
 
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(cors('https://app-loggie.onrender.com'))
 
 // CONF. PORTA
 app.listen(process.env.PORT || 3000, () => console.log("Servidor ligado! 🚀"))
