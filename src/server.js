750f09d786c585c79c45e5d13b4b733330960549
maxmalato-dell-3567 <max.malato@hotmail.com>
2024-10-22 13:41:07 -0300

    Atualizando a variável de desenvolvimento

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import cors from "cors"
 
 const app = express()
 app.use(express.json())
-app.use(cors('https://app-loggie.onrender.com'))
+app.use(cors())
 
 // CONF. PORTA
 app.listen(process.env.PORT || 3000, () => console.log("Servidor ligado! 🚀"))
